a0010a7381144550351dd572b53134c8bb499914
Archi903 <artpele@yandex.ru>
2022-12-08 12:53:26 +0300

    Задание 3

--- src/Main.java
+++ src/Main.java
@@ -2,20 +2,28 @@ public class Main {
     public static void main(String[] args) {
         task1();
         task2();
+        task3();
     }
 
     public static void task1() {
         System.out.println("Задание 1");
-        for (int i = 1; i <= 10; i ++) {
+        for (int i = 1; i <= 10; i++) {
             System.out.println("Итерация цикла " + i);
         }
     }
 
     public static void task2() {
         System.out.println("Задание 2");
-        for (int i = 10; i > 0; i --) {
+        for (int i = 10; i > 0; i--) {
             System.out.println("Итерация цикла " + i);
 
+        }
+    }
+
+    public static void task3() {
+        System.out.println("Задание 3");
+        for (int i = 0; i <= 17; i = i + 2) {
+            System.out.println("Итерация цикла " + i);
         }
     }
 }
