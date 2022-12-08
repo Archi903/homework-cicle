fc92d73fe73ccf5deaa95179148fcdb063a4ba71
Archi903 <artpele@yandex.ru>
2022-12-08 12:36:16 +0300

    Задание 1

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,10 @@ public class Main {
         task1 ();
     }
     public static void task1(){
-
+        System.out.println("Задание 1");
+        for (int i = 0; i <= 10;i = i + 1) {
+            System.out.println("Итерация цикла " + i);
+        }
     }
 
 }
